d11a3d19f63cff42520ff608fae0495777b7027d
nuaaz <nuaazh@outlook.com>
2025-05-04 22:47:01 +0800

    feat: remove template content and add 2 blogs

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -11,6 +11,7 @@ import {
   Instagram,
   Medium,
   Bluesky,
+  Leetcode,
 } from './icons'
 
 const components = {
@@ -26,6 +27,7 @@ const components = {
   instagram: Instagram,
   medium: Medium,
   bluesky: Bluesky,
+  leetcode: Leetcode,
 }
 
 type SocialIconProps = {
