08e7d1aedd2864eef67478c60d9f3d1159cd78af
nuaaz <nuaazh@outlook.com>
2025-06-23 20:38:29 +0800

    feature/add unsaved when updating blog

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: "Krob's Blog",
   author: 'Huan Zhang (krob)',
   headerTitle: "Krob's blog",
-  description: 'Tech interviews, coding journey, and personal reflections on software development',
+  description: 'Tech, coding journey, and personal',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
