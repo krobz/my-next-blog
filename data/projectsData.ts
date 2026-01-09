2afc02dba65d39c6b72364cfcf012b01a712240c
nuaaz <nuaazh@outlook.com>
2026-01-09 22:43:25 +0800

    feat: change sequence

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,16 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A 3D garment fitting system (Size guide in VIPSHOP)',
-    description: `A 3D garment fitting platform for VIPSHOP that analyzes purchase history to create virtual try-on models, reducing returns by providing accurate sizing.`,
-    imgSrc: '/static/images/3D_garment_system.png',
-    href: 'https://github.com/krobz/3d-garment-fitting-docs',
-  },
-  {
     title: 'TRPGHub - TRPG Session Management tool',
     description: `A mobile app connecting tabletop RPG game masters with players. Features service discovery, one-tap booking, split payments via QR codes, and real-time chat for session coordination.`,
     imgSrc: '/static/images/trpghub.png',
     href: 'https://www.trpghub.com/',
+  },
+  {
+    title: 'A 3D garment fitting system (Size guide in VIPSHOP)',
+    description: `A 3D garment fitting platform for VIPSHOP that analyzes purchase history to create virtual try-on models, reducing returns by providing accurate sizing.`,
+    imgSrc: '/static/images/3D_garment_system.png',
+    href: 'https://github.com/krobz/3d-garment-fitting-docs',
   },
 ]
 
